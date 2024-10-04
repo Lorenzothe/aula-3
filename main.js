f623c27720881f4e3a29cd0d9f933ed8276e974e
Lorenzothe <140451743+Lorenzothe@users.noreply.github.com>
2024-10-04 09:08:53 -0300

    a

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 let i = 0;
-while(i < length){
+while(i < estados.length){
     console.log(estados[i]);
     console.log(i);
     i++;
